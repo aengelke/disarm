d4e82826e49cd3bc9816e7784c214ab46bd6122f
Alexis Engelke <engelke@in.tum.de>
2024-06-21 21:16:40 +0200

    Initial public commit
    The actual initial commit was 2022-08-24. Given the large amount of code
    in okitec's farmdec, I wondered whether it is possible to generate a
    decoder/disassembler automatically from instruction descriptions.
    Originally, I only implemented a trie-based classifier -- which is
    extremely close to what is here now -- and a string-formatter, but no
    operand decoder ("go and extract bits yourself") or encoder. The
    classifier was extremely fast, but not very useful on its own.
    
    A few months later, I was working on AArch64 code generation and was in
    the need of a fast encoding library. Many years ago, I wrote a set of
    macros to encode a few AArch64 instructions -- so why not auto-generate
    this from the instruction descriptions? This was surprisingly easy,
    although it required a different set of mnemonics: when decoding, "ADC"
    is sufficient and not encoding operand sizes leads to fewer cases in
    switch statements. For the encoding API, however, I wanted registers to
    be plain integers and not carry (often useless) size information, so the
    mnemonics for encoding are "ADCw" and "ADCx". Encoding of immediates is
    non-trivial, especially for the logical operations AND/OR/EOR/ANDS/TST
    (see da_immlogical in encode.c).
    
    Another few months later (early 2024), we started working on the AArch64
    back-end of our own compiler framework. This provided motivation to
    actually completely cover the base ISA -- Advanced SIMD is a huge
    instruction set -- and important extensions. It also resulted
    performance improvements, most notably, the encoding functions are now
    (by default) inline functions in the header. Thus, the encoder has an
    extremely low performance cost.
    
    Now I had a more-or-less complete instruction encoding description,
    classifier, string-formatter (which was intensively fuzz-tested against
    LLVM-MC), and encoder ... but no usable decoder. So I went and annotated
    every instruction class with decode operand information and replaced the
    string-based formatter with an operand-based formatter. I also realized,
    how many operand types AArch64 has, especially in comparison to, e.g.,
    x86, which has around 10. The classifer can classify an instruction in
    about ~20 instructions and the operand decoder executes another 50-60
    instructions on average (x86-64 and AArch64, libLLVM.so) and can execute
    with an IPC of >2 (on both, Apple M1 and an older x86-64 Core). I think
    this is reasonably fast and it is acceptable to say that decoding is
    "fast enough". The formatter still has some room for perf improvements.
    
    Why did I decide to scrap 1.8 years of commit history? It had some
    lengthy holes and bulk commits, which are mostly an artefact of being a
    side project for some time (also reflects in the brevity of most commit
    messages). I also don't think there's valuable information in that
    history, and code that was removed was replaced with something that was
    superior in functionality. So here we are. Thank you for reading. <3

---
+++ test/decode-test.c
@@ -0,0 +1,86 @@
+
+#include <disarm64.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int main(void) {
+  const struct {
+    uint32_t inst;
+    unsigned mnem;
+    const char* disable_str;
+    const char* mnem_str;
+    const char* fmt;
+  } cases[] = {
+#define TESTF(cond, inst, mnem, fmt) {inst, mnem, cond ? 0 : #cond, #mnem, fmt},
+#define TEST(...) TESTF(1, __VA_ARGS__)
+
+      // clang-format off
+    TEST(0x00000000, DA64I_UDF, "udf #0x0")
+    TEST(0x1a000000, DA64I_ADC, "adc w0, w0, w0")
+    TEST(0x1a1f03ff, DA64I_ADC, "adc wzr, wzr, wzr")
+    TEST(0x9a000000, DA64I_ADC, "adc x0, x0, x0")
+    TEST(0x9a1f03ff, DA64I_ADC, "adc xzr, xzr, xzr")
+    TEST(0x3a000000, DA64I_ADCS, "adcs w0, w0, w0")
+    TEST(0x3a1f03ff, DA64I_ADCS, "adcs wzr, wzr, wzr")
+    TEST(0xba000000, DA64I_ADCS, "adcs x0, x0, x0")
+    TEST(0xba1f03ff, DA64I_ADCS, "adcs xzr, xzr, xzr")
+    TEST(0x5a000000, DA64I_SBC, "sbc w0, w0, w0")
+    TEST(0x5a1f03ff, DA64I_SBC, "sbc wzr, wzr, wzr")
+    TEST(0xda000000, DA64I_SBC, "sbc x0, x0, x0")
+    TEST(0xda1f03ff, DA64I_SBC, "sbc xzr, xzr, xzr")
+    TEST(0x7a000000, DA64I_SBCS, "sbcs w0, w0, w0")
+    TEST(0x7a1f03ff, DA64I_SBCS, "sbcs wzr, wzr, wzr")
+    TEST(0xfa000000, DA64I_SBCS, "sbcs x0, x0, x0")
+    TEST(0xfa1f03ff, DA64I_SBCS, "sbcs xzr, xzr, xzr")
+    TEST(0x0b204000, DA64I_ADD_EXT, "add w0, w0, w0, uxtw #0")
+    TEST(0x0b204c00, DA64I_ADD_EXT, "add w0, w0, w0, uxtw #3")
+    TEST(0x0b205000, DA64I_ADD_EXT, "add w0, w0, w0, uxtw #4")
+    TEST(0x0b205400, DA64I_UNKNOWN, "")
+    TEST(0x0b206000, DA64I_ADD_EXT, "add w0, w0, w0, uxtx #0")
+    TEST(0x8b206000, DA64I_ADD_EXT, "add x0, x0, x0, uxtx #0")
+    TEST(0x8b2063ff, DA64I_ADD_EXT, "add sp, sp, x0, uxtx #0")
+    TEST(0x8b20e000, DA64I_ADD_EXT, "add x0, x0, x0, sxtx #0")
+    TEST(0x8b3fe000, DA64I_ADD_EXT, "add x0, x0, xzr, sxtx #0")
+    TEST(0x8b204000, DA64I_ADD_EXT, "add x0, x0, w0, uxtw #0")
+    TEST(0xd4400000, DA64I_HLT, "hlt #0x0")
+    TEST(0x1e23c020, DA64I_FCVT, "fcvt h0, s1")
+    TESTF(DA64_HAVE_BF16, 0x1e634020, DA64I_BFCVT, "bfcvt h0, s1")
+  // clang-format on
+
+#include "decode-test-branchreg.inc"
+#include "decode-test-fmovimm.inc"
+#include "decode-test-immlogical.inc"
+#include "decode-test-immsimd.inc"
+#include "decode-test-pauth.inc"
+  };
+
+  char buf[128];
+  struct Da64Inst res;
+
+  printf("1..%zu\n", sizeof(cases) / sizeof(cases[0]));
+  for (size_t i = 0; i < sizeof(cases) / sizeof(cases[0]); i++) {
+    da64_decode(cases[i].inst, &res);
+    da64_format(&res, buf);
+
+    if (cases[i].disable_str) {
+      if (res.mnem == DA64I_UNKNOWN && buf[0] == '\0')
+        printf("ok %zu %08x %s (%s) # skip %s\n", i + 1, cases[i].inst,
+               cases[i].fmt, cases[i].mnem_str, cases[i].disable_str);
+      else
+        printf("not ok %zu %08x %s (%s) (!%s)\n", i + 1, cases[i].inst,
+               cases[i].fmt, cases[i].mnem_str, cases[i].disable_str);
+    } else {
+      int ok = res.mnem == cases[i].mnem && !strcmp(buf, cases[i].fmt);
+      printf("%sok %zu %08x %s (%s)\n", &"not "[4 * ok], i + 1, cases[i].inst,
+             cases[i].fmt, cases[i].mnem_str);
+      if (!ok) {
+        printf("# mnem: got=%#x expected=%#x (%s)\n", res.mnem, cases[i].mnem,
+               cases[i].mnem_str);
+        printf("# fmt: got=%s expected=%s\n", buf, cases[i].fmt);
+      }
+    }
+  }
+
+  return 0;
+}
---
+++ test/encode-test.c
@@ -0,0 +1,135 @@
+
+#include <disarm64.h>
+#include <inttypes.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static unsigned test_MOVconst(void) {
+  unsigned num = 0;
+  static const struct {
+    uint64_t cnst;
+    uint32_t enc[4];
+  } cases[] = {
+      {0x0000000000000000, {0xd2800000}}, // MOVZx
+      {0xffffffffffffffff, {0x92800000}}, // MOVNx
+      {0x0000000000000001, {0xd2800020}}, // MOVZx
+      {0xfffffffffffffffe, {0x92800020}}, // MOVNx
+      {0x0000000000001234, {0xd2824680}}, // MOVZx
+      {0xffffffffffffedcb, {0x92824680}}, // MOVNx
+      {0x000000000000ffff, {0xd29fffe0}}, // MOVZx
+      {0xffffffffffff0000, {0x929fffe0}}, // MOVNx
+      {0x0000000000010000, {0xd2a00020}}, // MOVZx16
+      {0xfffffffffffeffff, {0x92a00020}}, // MOVNx16
+      {0x0000000012340000, {0xd2a24680}}, // MOVZx16
+      {0xffffffffedcbffff, {0x92a24680}}, // MOVNx16
+      {0x00000000ffff0000, {0xd2bfffe0}}, // MOVZx16
+      {0xffffffff0000ffff, {0x92bfffe0}}, // MOVNx16
+      {0x000000000001ffff, {0xb24043e0}}, // ORR
+      {0xfffffffffffe0000, {0xb26fbbe0}}, // ORR
+      {0x000000000002ffff, {0x12bfffa0}}, // MOVNw16
+      // XXX: could use MOVN+MOVK
+      {0xfffffffffffd0000, {0xd2bfffa0, 0xf2dfffe0, 0xf2ffffe0}},
+      {0x000000001234ffff, {0x12bdb960}}, // MOVNw16
+      {0x00000000ffff0001, {0x129fffc0}}, // MOVNw
+      {0x00000000ffff1234, {0x129db960}}, // MOVNw
+      {0x1122334455667788, {0xd28ef100, 0xf2aaacc0, 0xf2c66880, 0xf2e22440}},
+      {0x0000334455667788, {0xd28ef100, 0xf2aaacc0, 0xf2c66880}},
+      {0x1122000055667788, {0xd28ef100, 0xf2aaacc0, 0xf2e22440}},
+      {0x1122334400007788, {0xd28ef100, 0xf2c66880, 0xf2e22440}},
+      {0x1122334455660000, {0xd2aaacc0, 0xf2c66880, 0xf2e22440}},
+      {0x1122334400000000, {0xd2c66880, 0xf2e22440}},
+      {0x1122000055660000, {0xd2aaacc0, 0xf2e22440}},
+  };
+  for (size_t i = 0; i < sizeof(cases) / sizeof(cases[0]); i++) {
+    const uint32_t* exp = cases[i].enc;
+    unsigned exp_cnt = !exp[1] ? 1 : !exp[2] ? 2 : !exp[3] ? 3 : 4;
+
+    uint32_t buf[4] = {0};
+    unsigned cnt = de64_MOVconst(buf, DA_GP(0), cases[i].cnst);
+    bool ok = cnt == exp_cnt && !memcmp(buf, exp, sizeof(buf));
+    num++;
+    printf("%sok %u MOVconst %d{%x,%x,%x,%x} %d{%x,%x,%x,%x} %016" PRIx64 "\n",
+           &"not "[4 * ok], num, exp_cnt, exp[0], exp[1], exp[2], exp[3], cnt,
+           buf[0], buf[1], buf[2], buf[3], cases[i].cnst);
+  }
+  return num;
+}
+
+static void test(unsigned num, uint32_t exp, uint32_t encoded,
+                 const char* name) {
+  bool ok = exp == encoded;
+  printf("%sok %u %08x %08x (%s)\n", &"not "[4 * ok], num, exp, encoded, name);
+}
+#define TEST(exp, encoded) test(++num, exp, encoded, #encoded)
+
+int main(void) {
+  unsigned num = 0;
+
+  num += test_MOVconst();
+
+#include "encode-test-adcsbc.inc"
+#include "encode-test-addsub-imm.inc"
+#include "encode-test-addsub-shift.inc"
+#include "encode-test-adr.inc"
+#include "encode-test-bfm-extr.inc"
+#include "encode-test-branchreg.inc"
+#include "encode-test-fcma.inc"
+#include "encode-test-hint.inc"
+#include "encode-test-immfmov.inc"
+#include "encode-test-immlogical.inc"
+#include "encode-test-immsimd.inc"
+#include "encode-test-mem.inc"
+#include "encode-test-memfp.inc"
+#include "encode-test-memsimd.inc"
+#include "encode-test-pauth.inc"
+#include "encode-test-simd-shr-vec-imm.inc"
+#include "encode-test-simdcopy.inc"
+#include "encode-test-simdxelem.inc"
+#include "encode-test-sys.inc"
+
+  // Unsorted tests
+  TEST(0x9b020c20, de64_MADDx(DA_GP(0), DA_GP(1), DA_GP(2), DA_GP(3)));
+  TEST(0x9b027c20, de64_MADDx(DA_GP(0), DA_GP(1), DA_GP(2), DA_ZR));
+  TEST(0x9b027c20, de64_MULx(DA_GP(0), DA_GP(1), DA_GP(2)));
+  TEST(0x9b028c20, de64_MSUBx(DA_GP(0), DA_GP(1), DA_GP(2), DA_GP(3)));
+  TEST(0x9b02fc20, de64_MSUBx(DA_GP(0), DA_GP(1), DA_GP(2), DA_ZR));
+  TEST(0x9b02fc20, de64_MNEGx(DA_GP(0), DA_GP(1), DA_GP(2)));
+  TEST(0xfa41300f, de64_CCMPx(DA_GP(0), DA_GP(1), 0xf, DA_LO));
+  TEST(0, de64_CCMPx(DA_GP(0), DA_GP(1), 0x10, DA_LO));
+  TEST(0, de64_CCMPx(DA_GP(0), DA_GP(1), -1, DA_LO));
+  TEST(0x9a823020, de64_CSELx(DA_GP(0), DA_GP(1), DA_GP(2), DA_CC));
+  TEST(0x9a823420, de64_CSINCx(DA_GP(0), DA_GP(1), DA_GP(2), DA_CC));
+  TEST(0xda823020, de64_CSINVx(DA_GP(0), DA_GP(1), DA_GP(2), DA_CC));
+  TEST(0xda823420, de64_CSNEGx(DA_GP(0), DA_GP(1), DA_GP(2), DA_CC));
+  TEST(0x9a813020, de64_CSELx(DA_GP(0), DA_GP(1), DA_GP(1), DA_CC));
+  TEST(0x9a813420, de64_CSINCx(DA_GP(0), DA_GP(1), DA_GP(1), DA_CC));
+  TEST(0x9a813420, de64_CINCx(DA_GP(0), DA_GP(1), DA_CS));
+  TEST(0xda813020, de64_CSINVx(DA_GP(0), DA_GP(1), DA_GP(1), DA_CC));
+  TEST(0xda813020, de64_CINVx(DA_GP(0), DA_GP(1), DA_CS));
+  TEST(0xda813420, de64_CSNEGx(DA_GP(0), DA_GP(1), DA_GP(1), DA_CC));
+  TEST(0xda813420, de64_CNEGx(DA_GP(0), DA_GP(1), DA_CS));
+  TEST(0x9a9f33e0, de64_CSELx(DA_GP(0), DA_ZR, DA_ZR, DA_CC));
+  TEST(0x9a9f37e0, de64_CSINCx(DA_GP(0), DA_ZR, DA_ZR, DA_CC));
+  TEST(0x9a9f37e0, de64_CSETx(DA_GP(0), DA_CS));
+  TEST(0xda9f33e0, de64_CSINVx(DA_GP(0), DA_ZR, DA_ZR, DA_CC));
+  TEST(0xda9f33e0, de64_CSETMx(DA_GP(0), DA_CS));
+  TEST(0xda9f37e0, de64_CSNEGx(DA_GP(0), DA_ZR, DA_ZR, DA_CC));
+  TEST(0xda9f37e0, de64_CNEGx(DA_GP(0), DA_ZR, DA_CS));
+
+  // SIMD logical
+  TEST(0x0ea11c20, de64_MOV8b(DA_V(0), DA_V(1)));
+  TEST(0x4ea11c20, de64_MOV16b(DA_V(0), DA_V(1)));
+
+  TEST(0x0f08a420, de64_SXTL_8h(DA_V(0), DA_V(1)));
+  TEST(0x0f10a420, de64_SXTL_4s(DA_V(0), DA_V(1)));
+  TEST(0x0f20a420, de64_SXTL_2d(DA_V(0), DA_V(1)));
+  TEST(0x4f08a420, de64_SXTL2_8h(DA_V(0), DA_V(1)));
+  TEST(0x4f10a420, de64_SXTL2_4s(DA_V(0), DA_V(1)));
+  TEST(0x4f20a420, de64_SXTL2_2d(DA_V(0), DA_V(1)));
+
+  printf("1..%u\n", num);
+  return 0;
+}
